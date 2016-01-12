b2613b05a3920adad6922379f6b25519c7887536
Hackbright Student <info@hackbrightacademy.com>
2016-01-12 13:00:31 -0800

    First 2 functions working, in progress on third function

--- markov.py
+++ markov.py
@@ -1,6 +1,5 @@
 from random import choice
 
-
 def open_and_read_file(file_path):
     """Takes file path as string; returns text as string.
 
@@ -10,7 +9,15 @@ def open_and_read_file(file_path):
 
     # your code goes here
 
-    return "This should be a variable that contains your file text as one long string"
+    text = open(file_path).read()
+
+    # print text 
+
+    # file_path.close()
+
+    return text
+
+# open_and_read_file("green-eggs.txt")
 
 
 def make_chains(text_string):
@@ -29,6 +36,24 @@ def make_chains(text_string):
     chains = {}
 
     # your code goes here
+    words = text_string.split()
+
+    # words[i], words[i+1]is a tuple and then add words[i+2] to a list  
+
+    for i in range(len(words) -1):
+        key = (words[i], words[i+1])
+        # print key 
+        # prints set of words in tuple form every combination 
+        # consecutive words 
+        try:
+            if key in chains:
+                chains[key].append(words[i+2])
+            else:
+                chains[key] = [words[i+2]]
+            # print chains 
+        except IndexError:
+            pass
+
 
     return chains
 
@@ -38,7 +63,23 @@ def make_text(chains):
 
     text = ""
 
+
     # your code goes here
+
+    # pull key from dictionary
+    # then pick random word from list assigned to the value 
+    # then add those 
+
+    for key in chains:
+        chosen_word = choice(chains[key])
+        new_tuple = (key[1], chosen_word)
+        print new_tuple
+
+        if new_tuple in chains:
+            chosen_word = choice(chains[new_tuple])
+
+        print chosen_word
+
 
     return text
 
@@ -47,6 +88,9 @@ input_path = "green-eggs.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
+# print "Input text next"
+# print input_text
+
 
 # Get a Markov chain
 chains = make_chains(input_text)
